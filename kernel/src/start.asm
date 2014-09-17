bd967b099f1c351acbc7832fa26edec48a7e6c95
ExeTwezz <exetwezz@mail.ru>
2014-09-17 18:25:21 +0400

    Update Makefile and add src directory

---
+++ kernel/src/start.asm
@@ -0,0 +1,52 @@
+; +------------------------+
+; | Basic Opetating System |
+; +------------------------+
+
+bits 32
+
+; The entry point.
+global entry
+entry:
+	mov esp, stack
+	jmp skip_header
+
+; The multiboot header.
+; ---------------------
+align 4
+header:
+	ALIGN_MODULES	equ 1 << 0
+	MEMORY_INFO	equ 1 << 1
+	AOUT_KLUDGE	equ 1 << 16
+
+	MAGIC		equ 0x1BADB002
+	FLAGS		equ ALIGN_MODULES | MEMORY_INFO | AOUT_KLUDGE
+	CHECKSUM	equ -(MAGIC + FLAGS)
+
+	dd MAGIC
+	dd FLAGS
+	dd CHECKSUM
+
+	extern code_start, bss_start, bss_end
+	dd header
+	dd code_start
+	dd bss_start
+	dd bss_end
+	dd entry
+;----------------------
+
+skip_header:
+	cli
+	
+	mov byte [0xB8000], 'P'
+	mov byte [0xB8001], 0x1B
+
+; Halt the CPU.
+global halt
+halt:
+	hlt
+	jmp halt
+
+; The stack.
+section .bss
+resb 8192
+stack:
