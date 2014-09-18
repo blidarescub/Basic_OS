08a6054bb7d80a43e0dc036f93d6af2aa2474a25
ExeTwezz <exetwezz@mail.ru>
2014-09-18 19:28:48 +0400

    Update kernel.c: add calling 'setup_idt()'

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,11 +2,15 @@
 // The C part of the kernel.
 
 #include <screen.h>
+#include <idt.h>
 
 // The C kernel main.
 void kernel_main (void)
 {
 	init_screen ();
 	clear_screen ();
+
+	setup_idt ();
+
 	puts ("Hello World");
 }
