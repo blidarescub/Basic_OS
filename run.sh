b318dacd446da3a25c8db07884672300f0f1658b
ExeTwezz <exetwezz@mail.ru>
2014-09-23 16:44:55 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-bochs -f bochs_config.txt
+DEFAULT="bochs"
+CURRENT="$1"
+
+if [ "$CURRENT" = "" ]; then
+	CURRENT="$DEFAULT"
+fi
+
+if [ "$CURRENT" = "bochs" ]; then
+	bochs -f bochs_config.txt
+elif [ "$CURRENT" = "qemu" ]; then
+	qemu-system-i386 -smp 1 -m 64 -fda disk/myos.img -boot a
+fi
