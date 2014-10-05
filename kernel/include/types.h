b83ffbde2150ab7cc67023e364d441c83a060262
ExeTwezz <exetwezz@mail.ru>
2014-10-05 08:17:16 +0400

    Update types.h: add the multiboot info structure

--- kernel/include/types.h
+++ kernel/include/types.h
@@ -25,4 +25,44 @@ typedef struct
 	u32 eip, cs, eflags, useresp, ss;
 } __attribute__ ((packed)) regs_irq_t;
 
+// Multiboot information structure.
+typedef struct
+{
+	u32 flags;
+
+	u32 mem_lower;
+	u32 mem_upper;
+
+	u32 boot_device;
+
+	u32 cmdline;
+
+	u32 mods_count;
+	u32 mods_addr;
+
+	u32 syms1;
+	u32 syms2;
+	u32 syms3;
+	u32 syms4;
+
+	u32 mmap_length;
+	u32 mmap_addr;
+
+	u32 drives_length;
+	u32 drives_addr;
+
+	u32 config_table;
+
+	u32 bootloader_name;
+
+	u32 apm_table;
+
+	u32 vbe_control_info;
+	u32 vbe_mode_info;
+	u32 vbe_mode;
+	u32 vbe_interface_seg;
+	u32 vbe_interface_off;
+	u32 vbe_interface_len;
+} __attribute__ ((packed)) mb_info_t;
+
 #endif /* !TYPES_H */
