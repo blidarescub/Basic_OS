6d15155ac3b3d96cec6430c2213fe3c2da8bb21f
ExeTwezz <exetwezz@mail.ru>
2014-10-06 18:53:42 +0400

    Update run.sh: add support for VirtualBox

--- run.sh
+++ run.sh
@@ -3,16 +3,20 @@
 # This script runs the specified (and supported) emulator.
 # "Supported" means that the code to start that emulator is written here.
 
-DEFAULT=bochs				# Default emulator.
-CURRENT=$DEFAULT			# Emulator to run.
+DEFAULT=virtualbox			# Default emulator/VM.
+CURRENT=$DEFAULT			# Emulator/VM to run.
 
-# Did the user specify what emulator to start?
+VBOX_VM_NAME="Basic OS"			# Place here the name of the virtual
+					# machine in which you want to run the
+					# Basic OS.
+
+# Did the user specify what emulator/VM to start?
 if [ ! "$1" = '' ]; then
-	CURRENT=$1			# Select the specified emulator.
+	CURRENT=$1			# Select the specified emulator/VM.
 fi
 
-# Run the emulator.
-if [ "$CURRENT" = bochs ]; then		# User wants to run in the Bochs?
+# Run the emulator/VM.
+if [ "$CURRENT" = bochs ]; then		# User wants to run the OS in Bochs?
 
 	# Is it installed?
 	if test $CURRENT; then
@@ -27,7 +31,7 @@ if [ "$CURRENT" = bochs ]; then		# User wants to run in the Bochs?
 		exit 1
 	fi
 
-elif [ "$CURRENT" = qemu ]; then	# User wants to run in the QEMU?
+elif [ "$CURRENT" = qemu ]; then	# User wants to run the OS in QEMU?
 
 	# Is it installed?
 	if test $CURRENT; then
@@ -42,6 +46,30 @@ elif [ "$CURRENT" = qemu ]; then	# User wants to run in the QEMU?
 		echo "on your system."
 		echo "Run the following command to install it:"
 		echo "$ sudo apt-get install qemu-system"
+		exit 1
+	fi
+
+elif [ "$CURRENT" = virtualbox ]; then	# User wants to run the OS in VBox?
+
+	vm_name=$VBOX_VM_NAME
+
+	# Is it installed?
+	if test $CURRENT; then
+		# Yes.
+		if ! virtualbox --startvm "$vm_name"; then
+			echo "The \`virtualbox\` command has returned 1."
+			echo -n "Maybe you didn't specify the name of the"
+			echo "virtual machine in which you want to run"
+			echo "the Basic OS:"
+			echo -n " > open the file 'run.sh' and set the "
+			echo "VBOX_VM_NAME variable."
+		fi
+	else
+		# No.
+		echo -n "The \`virtualbox\` package is not installed on your"
+		echo "system."
+		echo "Run the following command to install it:"
+		echo "$ sudo apt-get install virtualbox"
 	fi
 
 fi
